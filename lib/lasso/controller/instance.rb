4ff66b9e52af6201d46939f2af7f7f9da1db44a2
James Daniels <james@marginleft.com>
2010-06-26 13:01:59 -0400

    New version

--- lib/lasso/controller/instance.rb
+++ lib/lasso/controller/instance.rb
@@ -2,15 +2,18 @@ module Lasso
   module Controller
     module InstanceMethods
       def new
-        @oauth = oauth_settings[:for].call.send(oauth_model).new(:service => params[:service])
+        @oauth = type.new(:service => params[:service])
         redirect
       end
 
       def create
-        @oauth = oauth_settings[:for].call.send(oauth_model).new(:service => params[:service])
+        @oauth = type.new(:service => params[:service])
+        #@oauth = oauth_settings[:through].call.send(oauth_model).new(:service => params[:service])
         parse_response
-        if @oauth.save
-          redirect_to '/' #access_key_path(@oauth)
+        @owner = oauth_settings[:through].bind(self).call
+        nested = {"#{oauth_model}_attributes" => [@oauth.attributes]}
+        if @owner.update_attributes(nested)
+          redirect_to send("#{oauth_model.to_s.singularize}_path", @owner.send(oauth_model).last)
         else
           render :text => @oauth.to_yaml
         end
@@ -18,12 +21,16 @@ module Lasso
 
     protected
 
-      def oauth_callback_url
-        @oauth_callback_url ||= oauth_settings[:callback].call
+      def type
+        "OAuth#{version_one? && 'One' || 'Two'}#{oauth_model_constant}".constantize
+      end
+      
+      def oauth_model_constant
+        oauth_model.to_s.singularize.camelcase.constantize
       end
 
       def version_one?
-        @version_one ||= @oauth.config(:oauth_version) == 1
+        @version_one ||= oauth_model_constant.oauth_providers[params[:service]][:oauth_version] == 1
       end
 
       def parse_response
@@ -32,18 +39,18 @@ module Lasso
           access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
           @oauth.attributes = {:oauth_token => access_token.params[:oauth_token], :oauth_token_secret => access_token.params[:oauth_token_secret]}
         else
-          access_token = @oauth.client.web_server.get_access_token(params[:code], :redirect_uri => oauth_callback_url)
+          access_token = @oauth.client.web_server.get_access_token(params[:code], :redirect_uri => oauth_settings[:callback].bind(self).call)
           @oauth.attributes = {:refresh_token => access_token.refresh_token, :access_token => access_token.token}
         end
       end
 
       def redirect
         if version_one?
-          @request_token = @oauth.consumer.get_request_token(:oauth_callback => oauth_callback_url)
+          @request_token = @oauth.consumer.get_request_token(:oauth_callback => oauth_settings[:callback].bind(self).call)
           session[:request_token] = @request_token
           redirect_to @request_token.authorize_url
         else
-          redirect_to @oauth.client.web_server.authorize_url(:redirect_uri => oauth_callback_url)
+          redirect_to @oauth.client.web_server.authorize_url(:redirect_uri => oauth_settings[:callback].bind(self).call)
         end
       end
     end
--- lib/lasso/model/instance.rb
+++ lib/lasso/model/instance.rb
@@ -5,7 +5,7 @@ module Lasso
         oauth_providers[service][key]
       end
       def set_type
-        self.type = "OAuth#{config(:oauth_version) == 1 && 'One' || 'Two'}#{self.class.to_s}"
+        self.type ||= "OAuth#{config(:oauth_version) == 1 && 'One' || 'Two'}#{self.class.to_s}"
       end
     end
   end
